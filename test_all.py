8061d6cc3e6a7fb5c923ae9b1ee4c721d1959364
Druva D <ddruva445@gmail.com>
2023-06-21 00:18:36 +0530

    Added error throw

--- test_all.py
+++ test_all.py
@@ -5,4 +5,10 @@ folders = os.listdir()
 
 for folder in folders:
     if folder.startswith("Lab"):
-        test = subprocess.Popen(["./test.sh", folder])
+        test = subprocess.Popen(["./test.sh", folder], stdout=subprocess.PIPE)
+        output = test.communicate()[1]
+        if test.returncode != 0:
+            # print(output.decode("utf-8"))
+            raise Exception("Test failed for " + folder)
+
+        print("Testing " + folder)
